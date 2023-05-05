e81c15dde994752104e7d76dab079af5999f2311
Muhammed Bilal <bilalmuhammed363@gmail.com>
2023-05-05 11:29:58 +0530

    implimented firebase in admin ap

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shoe_rack_ecommerce_admin/presentation/screens/login_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -29,4 +32,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
